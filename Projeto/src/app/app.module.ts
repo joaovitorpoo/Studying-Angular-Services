85a9219607923d090d5847614ee952bd5b1aca7e
João Vitor <63071327+joaovitorpoo@users.noreply.github.com>
2020-04-29 20:42:28 -0300

    Creating Component

--- Projeto/src/app/app.module.ts
+++ Projeto/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PessoasComponent } from './pessoas/pessoas.component';
+import { PessoasDetalheComponent } from './pessoas-detalhe/pessoas-detalhe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PessoasComponent,
+    PessoasDetalheComponent
   ],
   imports: [
     BrowserModule
